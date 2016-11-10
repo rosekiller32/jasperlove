79a2e673be2f310ef7daa3f1850f37cbe3480d0a
rosekiller32 <rosekiller32@gmail.com>
2016-11-10 23:22:04 +0800

    init

--- js/main.js
+++ js/main.js
@@ -25,8 +25,13 @@ var fbProvider = new firebase.auth.FacebookAuthProvider();
   firebase.auth().onAuthStateChanged(function (user) {
     if(user){
       currentUser=user;
+      $("#form").css("display","block");
+      $("#status").text("請輸入你的題目！！ex:柏璋愛打砲～");
     }else{
       currentUser=null;
+      $("#form").css("display","none");
+      $("#status").text("請先登入fb帳號");
+      
     } 
   })
 
@@ -42,13 +47,22 @@ $("#signin").click(function () {
       data["/users/"+uid+"/photo"]= currentUser.photoURL;
       firebase.database().ref().update(data);
       logginOption(true);
-      location.reload();
+      
+      //location.reload();
   }).catch(function (error) {
     var errorCode = error.code;
     var errorMessa = error.message;
     console.log(errorCode,errorMessa);
   })
 
+});
+$("#Submit").click(function () {
+    var text = {};
+    var uid=currentUser.uid;
+    text["/users/"+uid+"/text"]=$("#exampleTextarea").val();
+    firebase.database().ref().update(text);
+    form.style.display='none';
+    $("#status").text("謝謝你的參加，柏璋愛你喔！！");
 });
 
 
@@ -63,23 +77,23 @@ $("#signout").click(function () {
 
 });
 
-$("#submitData").click(function () {
-    // 上傳新商品
-    var name=$("#itemName").val();
-    var price=$("#price").val();
-    var picData=$("#picData").val();
-    var descrip=$("#descrip").val();
-    firebase.database().ref("item").push({name: name,price: price,descrip: descrip,picData: picData});
-})
+// $("#submitData").click(function () {
+//     // 上傳新商品
+//     var name=$("#itemName").val();
+//     var price=$("#price").val();
+//     var picData=$("#picData").val();
+//     var descrip=$("#descrip").val();
+//     firebase.database().ref("item").push({name: name,price: price,descrip: descrip,picData: picData});
+// })
 
-$("#editData").click(function () {
-    // 編輯商品資訊
+// $("#editData").click(function () {
+//     // 編輯商品資訊
 
-})
+// })
 
-$("#removeData").click(function () {
-    //刪除商品
-})
+// $("#removeData").click(function () {
+//     //刪除商品
+// })
 
 
 /*
@@ -105,83 +119,83 @@ function logginOption(isLoggin) {
 }
 
 
-function reProduceAll(allItems) {
-  /*
-  清空頁面上 (#item)內容上的東西。
-  讀取爬回來的每一個商品
-  */
-  $("#item").empty();  
-  /*
-    利用for in存取
-  */
-  console.log(allItems.val());
+// function reProduceAll(allItems) {
+//   /*
+//   清空頁面上 (#item)內容上的東西。
+//   讀取爬回來的每一個商品
+//   */
+//   $("#item").empty();  
+  
+//     利用for in存取
+  
+//   console.log(allItems.val());
 
-  for (var item in allItems.val()) {
-    produceSingleItem(allItems.val()[item],item);
-  }
-}
-
-
-
-//每點開一次就註冊一次
-function produceSingleItem(sinItemData,item){
-  /*
-    抓取 sinItemData 節點上的資料。
-    若你的sinItemData資料欄位中並沒有使用者名稱，請再到user節點存取使用者名稱
-    資料齊全後塞進item中，創建 Item 物件，並顯示到頁面上。
-  */
-  console.log(sinItemData);
-  firebase.database().ref().once("value",function () {
-  var currentUser = firebase.auth().currentUser;
-  var product = new Item({title: "cup", price: 1200, itemKey: "-sdfjiotfdgjl", seller: "sdfjigalngme", sellerName:"Radia"}, currentUser);
-
-  $("#items").append(product);
-
-      /*
-        用 ViewModal 填入這筆 item 的資料
-        呼叫 ViewModal callImage打開圖片
-        創建一個 MessageBox 物件，將 Message 的結構顯示上 #message 裡。
-      */
-      var viewModal = new ViewModal($(“#view-modal”));
-      var uploadModal = new UploadModal($(“#upload-modal”));
-      uploadModal.submitPic(firebase.auth().currentUser.uid);
+//   for (var item in allItems.val()) {
+//     produceSingleItem(allItems.val()[item],item);
+//   }
+// }
 
 
-    $("#message").append();
+
+// //每點開一次就註冊一次
+// function produceSingleItem(sinItemData,item){
+//   /*
+//     抓取 sinItemData 節點上的資料。
+//     若你的sinItemData資料欄位中並沒有使用者名稱，請再到user節點存取使用者名稱
+//     資料齊全後塞進item中，創建 Item 物件，並顯示到頁面上。
+//   */
+//   console.log(sinItemData);
+//   firebase.database().ref().once("value",function () {
+//   var currentUser = firebase.auth().currentUser;
+//   var product = new Item({title: "cup", price: 1200, itemKey: "-sdfjiotfdgjl", seller: "sdfjigalngme", sellerName:"Radia"}, currentUser);
+
+  // $("#items").append(product);
+
+  //     /*
+  //       用 ViewModal 填入這筆 item 的資料
+  //       呼叫 ViewModal callImage打開圖片
+  //       創建一個 MessageBox 物件，將 Message 的結構顯示上 #message 裡。
+  //     */
+  //     var viewModal = new ViewModal($(“#view-modal”));
+  //     var uploadModal = new UploadModal($(“#upload-modal”));
+  //     uploadModal.submitPic(firebase.auth().currentUser.uid);
+
+
+  //   $("#message").append();
 
       
-        // 判斷使用者是否有登入，如果有登入就讓 #message 容器顯示輸入框。
-        // 在 MessageBox 上面註冊事件，當 submit 時將資料上傳。
-        var messages = new MessageBox(firebase.auth().currentUser, itemKey); 
-        messages.addDialog({message:"", time: 1487529, name: "Radia", picURL:"http:......."});
+  //       // 判斷使用者是否有登入，如果有登入就讓 #message 容器顯示輸入框。
+  //       // 在 MessageBox 上面註冊事件，當 submit 時將資料上傳。
+  //       var messages = new MessageBox(firebase.auth().currentUser, itemKey); 
+  //       messages.addDialog({message:"", time: 1487529, name: "Radia", picURL:"http:......."});
       
-    if (currentUser) {
-      $("#message").append(messBox.inputBox);
-      messBox.inputBox.keypress(function (e) {
-      if (e.which == 13) {
-          e.preventDefault();
+  //   if (currentUser) {
+  //     $("#message").append(messBox.inputBox);
+  //     messBox.inputBox.keypress(function (e) {
+  //     if (e.which == 13) {
+  //         e.preventDefault();
 
-            /*
-            取得input的內容 $(this).find("#dialog").val();
-            清空input的內容 $(this).find("#dialog").val("");
-            */
+  //           /*
+  //           取得input的內容 $(this).find("#dialog").val();
+  //           清空input的內容 $(this).find("#dialog").val("");
+  //           */
 
-        }
-      });
-    }
+  //       }
+  //     });
+  //   }
 
     /*
     從資料庫中抓出message資料，並將資料填入MessageBox
     */
-    firebase.database().ref().orderBy("value",function(data) {
+    // firebase.database().ref().orderBy("value",function(data) {
 
-    });
-  });
+    // });
+  // });
     /*
     如果使用者有登入，替 editBtn 監聽事件，當使用者點選編輯按鈕時，將資料顯示上 uploadModal。
     */
 
-}
+// }
 
 function generateDialog(diaData, messageBox) {
 
